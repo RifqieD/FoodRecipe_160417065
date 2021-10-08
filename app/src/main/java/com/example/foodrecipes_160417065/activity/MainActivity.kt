acf338c8369a67693d0faecaa01a057d0932e7d0
rifqie dwi a <rifqie241099@gmail.com>
2021-10-08 21:04:23 +0700

    commit

---
+++ app/src/main/java/com/example/foodrecipes_160417065/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.foodrecipes_160417065.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.foodrecipes_160417065.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
